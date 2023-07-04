43dff3cfd4df3415a4b4bac1658039c8bcd6c24d
PaoloAttardi <paolo.attardi@student.univaq.it>
2023-07-04 12:04:26 +0200

    path from one stop to another

--- main.py
+++ main.py
@@ -1,55 +1,51 @@
-from matplotlib import pyplot as plt
-import peartree as pt
-import networkx as nx
-from networkx.drawing.nx_pydot import write_dot
 from neo4j import GraphDatabase
+from datetime import datetime
 
-gtfs_file_path = "GTFS_230406_240405.zip"
 uri = "bolt://localhost:7687"
 username = "neo4j"
 password = "password"
 
-# Carica il feed GTFS utilizzando peartree
-feed = pt.get_representative_feed(gtfs_file_path)
-trips = feed.trips
-tmp = trips[trips['trip_id'] == '1022_10']
-print(tmp)
+# Coordinate del dipartimento di ingegneria di Modena
+lat = 44.6293839463985
+lon = 10.948827844203144
 
-'''routes = feed.routes
-trips = feed.trips
-stops = feed.stops
-stop_times = feed.stop_times
-agency = feed.agency'''
-
-# Genera il grafo
-start = 6*60*60 + 58*60  # 6:58 AM
-end = 7*60*60  # 7:00 AM
-graph = pt.load_feed_as_graph(feed,start,end) # graph è un multigrafo --> si può aggiungere lo stesso edge più volte
-
-# pt.generate_plot(graph)
-# write_dot(graph, 'graph.dot')
-pos = nx.spring_layout(graph)
-nx.draw(graph, pos, with_labels=True, connectionstyle='arc3, rad = 0.1')
-edge_labels=dict([((u,v,),d['length'])
-             for u,v,d in graph.edges(data=True)])
-
-# plt.show()
-plt.savefig('graph.png')
-
-# Visualizza il contenuto dei nodi del grafo
-for node in graph.nodes(data=True):
-    id,data = node
-    print(id,data)
-    break
-
-for edge in graph.edges(data=True):
-    node1,node2,data = edge
-    print(f'Edge che collega {node1} a {node2} con i seguenti dati {data}')
-    print(data['mode'])
-    break
+# Nome della fermata di arrivo
+dest = "GARIBALDI"
 
 # Connessione al database Neo4j
-# driver = GraphDatabase.driver(uri, auth=(username, password))
+driver = GraphDatabase.driver(uri, auth=(username, password))
 
-# with driver.session() as session:
-#    print('Connected')
+with driver.session() as session:
+    print('Connected')
+
+    # Prendi il nodo più vicino alle coordinate date
+    query = """MATCH (s:Stop)
+            WITH point({x: $lat, y: $lon, crs: 'cartesian'}) AS p1, point({x: s.lat, y: s.lon, crs: 'cartesian'}) AS p2, s
+            RETURN s, point.distance(p1,p2) AS dist ORDER BY dist ASC LIMIT 1"""
+    result = session.run(query, lat=lat, lon=lon)
+ 
+    for record in result:
+        lat,lon,name = record["s"].get('lat'), record["s"].get('lon'), record["s"].get('name')
+
+    # Ottieni l'orario attuale
+    now = datetime.now()
+    formatted_time = now.strftime('%H:%M:%S')
+
+    # Calcola il percorso per raggiungere la fermata desiderata
+    query = """MATCH p=(s:Stop {name:$name})-[:LOCATED_AT]-(s1:Stoptime)-[:PART_OF_TRIP]-(t1:Trip)-[:USES]-(:Route)-[:USES]-(t2:Trip)-[:PART_OF_TRIP]-(s2:Stoptime)-[:LOCATED_AT]-(f:Stop {name:$dest})
+            WHERE s1.departure_time >= $formatted_time AND s1.departure_time < s2.arrival_time RETURN p LIMIT 1"""
+    result = session.run(query, name=name, formatted_time=formatted_time, dest=dest)
+
+    for record in result:
+        path = record["p"]
+    
+    # Iterazione attraverso i nodi e le relazioni all'interno del percorso
+    i = 'Partenza'
+    for node in path.nodes:
+        if node["name"] != None:
+            print(f"Fermata di {i}:", node["name"])
+        elif node["short_name"] != None:
+            print("Tratta Autobus:", node["short_name"])
+        elif node["departure_time"] != None:
+            print(f"Orario di {i}:", node["departure_time"])
+            i = 'Arrivo'
