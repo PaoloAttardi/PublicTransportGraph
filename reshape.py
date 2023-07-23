d7a08c069e6c081d76de87b594ee8f0f99825c5b
PaoloAttardi <paolo.attardi@student.univaq.it>
2023-07-23 12:16:25 +0200

    Modifica alla generazione del file new_calendar

--- reshape.py
+++ reshape.py
@@ -1,18 +1,35 @@
 # script per modificare il file calendar_dates.txt
 
-input_file = 'GTFS_230406_240405\calendar_dates.txt'
+input_file = 'GTFS_230406_240405/calendar_dates.txt'
+trip_file = 'GTFS_230406_240405/trips.txt'
 output_file = 'new_calendar_dates.txt'
-header = 'service_id,year,month,day,exception_type'
+header = 'service_id,day,exception_type'
 
+with open(trip_file, 'r') as trips:
+    for line_num, line in enumerate(trips):
+        if line_num == 0:
+            # Istanzia la lista per memorizzare i service_id
+            service_id = []
+        else:
+            # Aggiunge un nuovo service_id nella lista
+            parts = line.strip().split(',')
+            if parts[1] not in service_id:
+                service_id.append(parts[1])
+
+print(f'I service_id utilizzati nel file trips sono: {len(service_id)}')
+print(f'E sono i seguenti: {service_id}')
+
+print('Modifico il file Calendar_dates.txt in un nuovo file new_calendar_dates.txt valutando solo i trips utilizzati...')
 with open(input_file, 'r') as file_in, open(output_file, 'w') as file_out:
     for line_num, line in enumerate(file_in):
         if line_num == 0:
             # Scrive la prima riga senza modifiche nel file di output
-            file_out.write(header)
+            file_out.write(header + '\n')
         else:
             # Modifica le righe successive differenziando l'anno il mese e il giorno
             parts = line.strip().split(',')
-            date = parts[1]
-            modified_date = f"{date[:4]},{date[4:6]},{date[6:]}"
-            modified_line = f"{parts[0]},{modified_date},{parts[2]}"
-            file_out.write(modified_line + '\n')
+            if parts[0] in service_id:
+                date = parts[1]
+                modified_date = f"{date[:4]}-{date[4:6]}-{date[6:]}"
+                modified_line = f"{parts[0]},{modified_date},{parts[2]}"
+                file_out.write(modified_line + '\n')
